dfff28c0bae3bb1eeb606d1b9235ab55d170f3fd
ManhAnNguyen <thungantn1999@gmail.com>
2024-03-12 20:16:44 +0700

    add day15

--- README.md
+++ README.md
@@ -1,15 +1,2 @@
-LESSON 13 : CUSTOM HOOKS
-
-useState,useEffect,useMemo,useCallback,useRef,useContext,......
-
-1 :quy tắc dùng hook
-2 :custom hook
-
-dùng custom hook khi mã code nó lặp lại,(giống, tương tự) ở các component
-
-custom hook
-
-increase
-decrease
-multiply (2) => \*2
-divide (2) => /2
+Form : React Hook Form
+Validate :Zod
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,91 @@
 import { Button, Input } from "antd";
-import React, { useState } from "react";
-import Header from "./components/Header";
-import Body from "./components/Body";
-import useInput from "./hooks/useInput";
+import React from "react";
+import { useFormik } from "formik";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup.string().required(),
+  confirmationPassword: yup.string().required(),
+});
 
 const App = () => {
-  const { value, onChange, reset } = useInput();
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+      confirmationPassword: "",
+    },
+
+    onSubmit: (value) => {
+      toast.success(`Welcome ${value.username}`);
+    },
+    validationSchema,
+    validate: ({ password, confirmationPassword }) => {
+      if (password !== confirmationPassword) {
+        return { confirmationPassword: "please check password validation" };
+      }
+    },
+  });
+
+  const {
+    username: usernameError,
+    password: passwordError,
+    confirmationPassword: confirmationPasswordErr,
+  } = formik.errors;
   return (
-    <div>
-      <h1>{value}</h1>
-      <Input
-        value={value}
-        onChange={onChange}
-        suffix={
-          <Button type="primary" danger size="small" onClick={reset}>
-            Reset
-          </Button>
-        }
-      />
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <label>Username</label>
+        <Input
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          name="username"
+          status={usernameError && "error"}
+        />
+        {usernameError && <p className="error">{usernameError}</p>}
+      </div>
+      <div>
+        <label>Password</label>
+        <Input
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          name="password"
+          status={passwordError && "error"}
+        />
+        {passwordError && <p className="error">{passwordError}</p>}
+      </div>
+
+      <div>
+        <label>Confirmation Password</label>
+        <Input
+          value={formik.values.confirmationPassword}
+          onChange={formik.handleChange}
+          name="confirmationPassword"
+          status={confirmationPasswordErr && "error"}
+        />
+        {confirmationPasswordErr && (
+          <p className="error">{confirmationPasswordErr}</p>
+        )}
+      </div>
+      <Button
+        type="primary"
+        style={{
+          marginTop: "10px",
+        }}
+        htmlType="submit"
+      >
+        Sign up
+      </Button>
+
+      <Button type="primary" danger onClick={formik.handleReset}>
+        Reset
+      </Button>
+      <ToastContainer />
+    </form>
   );
 };
-
 export default App;
